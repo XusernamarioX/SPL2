f7e84d4911afa9c6883a33e8d5d9c3de0dd0340b
XusernamarioX <171wita11@o365.htl-leoben.at>
2018-04-09 14:57:50 +0200

    ja is ok

--- bus-und-haltestelle.py
+++ bus-und-haltestelle.py
@@ -3,7 +3,8 @@ eingabe = int(eingabe)
 for i in range(1,eingabe+1):
     eingabe2 = input("Bitte personen eingeben:")
     eingabe2 = eingabe2
+    eingabe3 = input("Steigen welche aus?")
+    aussteigen = eingabe2-eingabe3
     print("Bei ",eingabe , " Haltestellen sind ",eingabe2 , " Personen eingestiegen")
-
-if(eingabe2 == 60):
-    print("BUS VOLL!")
+    if(eingabe2 == 60):
+        print("Bus voll!")
