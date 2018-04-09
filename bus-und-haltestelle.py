7a877f5076f7ca520ac6c24ba8b0a4a9eae8e898
XusernamarioX <171wita11@o365.htl-leoben.at>
2018-04-09 15:26:05 +0200

    askla

--- bus-und-haltestelle.py
+++ bus-und-haltestelle.py
@@ -4,7 +4,9 @@ for i in range(1,eingabe+1):
     eingabe2 = input("Bitte personen eingeben:")
     eingabe2 = eingabe2
     eingabe3 = input("Steigen welche aus?")
-    aussteigen = eingabe2-eingabe3
+    aussteigen = int(eingabe3)
+    if(aussteigen):
+        print("Es steigen ",eingabe3 ," Personen aus")
     print("Bei ",eingabe , " Haltestellen sind ",eingabe2 , " Personen eingestiegen")
     if(eingabe2 == 60):
         print("Bus voll!")
