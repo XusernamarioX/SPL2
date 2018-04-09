bfb00242edeb7352c5eda594e152cabbe0dccd2a
XusernamarioX <171wita11@o365.htl-leoben.at>
2018-04-09 14:52:11 +0200

    everything oge

--- bus-und-haltestelle.py
+++ bus-und-haltestelle.py
@@ -4,3 +4,6 @@ for i in range(1,eingabe+1):
     eingabe2 = input("Bitte personen eingeben:")
     eingabe2 = eingabe2
     print("Bei ",eingabe , " Haltestellen sind ",eingabe2 , " Personen eingestiegen")
+
+if(eingabe2 == 60):
+    print("BUS VOLL!")
