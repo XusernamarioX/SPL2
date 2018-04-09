d06dec813126a24e697a778e3cc9ee5657927da7
XusernamarioX <171wita11@o365.htl-leoben.at>
2018-04-09 14:47:48 +0200

    erste uebung

--- Bus&Haltestelle.py
+++
@@ -1,6 +0,0 @@
-eingabe = input("Bitte Anzahl der Haltestellen eingeben:")
-eingabe = int(eingabe)
-for i in range(1,eingabe+1):
-    eingabe2 = input("Bitte personen eingeben:")
-    eingabe2 = eingabe2
-    print("Bei ",eingabe , " Haltestellen sind ",eingabe2 , " Personen eingestiegen")
